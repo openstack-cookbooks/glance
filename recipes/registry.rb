0fdf474333e374b55fb3fa3452b79a2834974499
Choe, Cheng-Dae <whitekid@gmail.com>
2014-03-12 02:43:00 +0900

    기본 패키지만 설치

---
+++ recipes/registry.rb
@@ -0,0 +1,8 @@
+package "glance-registry"
+
+ervice 'glance-registry' do
+    provider Chef::Provider::Service::Upstart
+    supports :status => :true, :restart => :true, :reload => :true
+    action [:enable, :start]
+end
+
